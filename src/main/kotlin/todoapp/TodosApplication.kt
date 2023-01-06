5bd1f74f44d66f12b96dce163d1094ac1e58927a
Jehaechan <71350045+Jehaechan@users.noreply.github.com>
2023-01-06 12:56:56 +0900

    init

---
+++ src/main/kotlin/todoapp/TodosApplication.kt
@@ -0,0 +1,12 @@
+package todoapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodosApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodosApplication>(*args)
+}
+
